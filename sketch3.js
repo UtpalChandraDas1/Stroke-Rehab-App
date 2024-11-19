64d652fe79ac949addb886914babfb80ee30dc54
Utpal Chandra Das <88704641+UtpalChandraDas1@users.noreply.github.com>
2024-11-19 14:41:49 +0700

    Update sketch3.js

--- sketch3.js
+++ sketch3.js
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pose Estimation for Left Hip Flexion</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="styles33.css">
 </head>
 <body>
     <h1>Pose Estimation Web Application: Left Hip Flexion</h1>
